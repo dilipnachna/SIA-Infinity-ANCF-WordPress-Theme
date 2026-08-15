e90bd31e2afd2119fa45b07b129ad69be5de9949
Dilip Soni <dilipnachna@gmail.com>
2026-08-15 23:45:50 +0530

    Sync ANCF Core version with v0.3 staging release

--- plugins/sia-ancf-core/sia-ancf-core.php
+++ plugins/sia-ancf-core/sia-ancf-core.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: SIA ANCF Core
  * Description: Publisher-intelligence foundation for SIA Infinity ANCF.
- * Version: 0.2.0-alpha.1
+ * Version: 0.3.0-alpha.1
  * Requires PHP: 8.1
  * Author: SIA Infinity
  * Text Domain: sia-ancf-core
@@ -12,7 +12,7 @@ if (!defined('ABSPATH')) {
     exit;
 }
 
-define('SIA_ANCF_CORE_VERSION', '0.2.0-alpha.1');
+define('SIA_ANCF_CORE_VERSION', '0.3.0-alpha.1');
 define('SIA_ANCF_RUNTIME_MODE', 'publisher-intelligence');
 
 require_once __DIR__ . '/includes/class-sia-ancf-core.php';
